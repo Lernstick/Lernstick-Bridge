6bf8e49b1ee59db4cbf4f34259862554a8ca9e41
Thore Sommer <mail@thson.de>
2022-12-09 13:49:47 +0200

    lernstick2ima: add option for keys and better handling of hashes

--- lernstick2ima.py
+++ lernstick2ima.py
@@ -1,21 +1,24 @@
 #!/bin/env python3
 '''
 SPDX-License-Identifier: AGPL-3.0-only
-Copyright 2021 Thore Sommer
+Copyright 2022 Thore Sommer
 
 Extracts the necessary information from a Lernstick ISO for generating a very simple IMA policy
 Needs the following tools installed
  - unmkinitramfs
 '''
+import base64
 import glob
 import json
 import os
 import argparse
-import shutil
 import subprocess
 import hashlib
 
 from tempfile import TemporaryDirectory
+
+from cryptography.hazmat.primitives import serialization
+from cryptography.x509 import load_pem_x509_certificate, ExtensionNotFound, oid
 
 
 def mount(iso, mount_dir):
@@ -31,9 +34,11 @@ def unmount(mount_dir):
         raise Exception(f"Failed to unmount ISO: {res.stderr}")
 
 
-def hash_files(dir):
+def hash_files(dir, rel_dir=None):
+    if not rel_dir:
+        rel_dir = dir
     output = {}
-    for file in glob.glob(f"{dir}/**/*", recursive=True):
+    for file in glob.glob(f"{dir}/**/*", recursive=True) + [dir]:
         if not os.path.isfile(file):
             continue
 
@@ -42,9 +47,11 @@ def hash_files(dir):
             continue
 
         with open(file, 'rb') as f:
-            hash = hashlib.sha256(f.read()).hexdigest()
-        name = f'/{os.path.relpath(file, dir)}'
-        output[name] = [hash]
+            data = f.read()
+            hash_sha1 = hashlib.sha1(data).hexdigest()
+            hash_sha256 = hashlib.sha256(data).hexdigest()
+        name = f'/{os.path.relpath(file, rel_dir)}'
+        output[name] = [hash_sha1, hash_sha256]
     return output
 
 
@@ -54,15 +61,62 @@ def hash_initramfs(initfs, dest_dir):
     return {**hash_files(f'{dest_dir}/main'), **hash_files(f'{dest_dir}/early')}
 
 
+def encode_pub_key(key):
+    fmt = serialization.PublicFormat.SubjectPublicKeyInfo
+    pubbytes = key.public_bytes(encoding=serialization.Encoding.DER, format=fmt)
+    return base64.b64encode(pubbytes).decode("ascii")
+
+
+def get_keyidv2_from_cert(cert):
+    """Get the keyidv2 from the cert's Subject Key Identifier (SKID) if available."""
+    if cert.extensions:
+        try:
+            skid = cert.extensions.get_extension_for_oid(oid.ExtensionOID.SUBJECT_KEY_IDENTIFIER)
+            if skid and skid.value and len(skid.value.digest) >= 4:
+                keyidv2 = int.from_bytes(skid.value.digest[-4:], "big")
+                return keyidv2
+        except ExtensionNotFound:
+            pass
+    return None
+
+
+def parse_keys(keys):
+    key_ids = []
+    pubkeys = []
+
+    for key_path in keys:
+        with open(key_path, "rb") as f:
+            filedata = f.read()
+            cert = load_pem_x509_certificate(filedata)
+            pubkey = encode_pub_key(cert.public_key())
+            keyid = get_keyidv2_from_cert(cert)
+            pubkeys.append(pubkey)
+            key_ids.append(keyid)
+    return {
+        "keyids": key_ids,
+        "pubkeys":  pubkeys
+    }
+
+def merge_dict(from_dict, to_dict):
+    for key, value in from_dict.items():
+        if key in to_dict:
+            to_dict[key].extend(value)
+        else:
+            to_dict[key] = value
+
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--key", "-k", action="append", help="Use IMA keys instead of generated hash list. "
+                                                                      "Has better performance. Needs to be PEM encoded.")
+    parser.add_argument("--exclude", "-e", action='append', help="List of regexes to exclude")
+    parser.add_argument("--exclude-initramfs", "-i", action="store_true")
     parser.add_argument("lernstickISO")
     args = parser.parse_args()
 
     if os.getuid() != 0:
             exit("This script mus be run as root!")
 
-    policy = {}
+    hash_list = {}
     with TemporaryDirectory() as tempdir:
         try:
             iso_dir = f'{tempdir}/iso'
@@ -70,13 +124,21 @@ def main():
             init_dir = f'{tempdir}/init'
             mount(args.lernstickISO, iso_dir)
             mount(f'{iso_dir}/live/filesystem.squashfs', squash_dir)
-            policy.update(hash_files(squash_dir))
-            for key, value in hash_initramfs(f'{iso_dir}/live/initrd.img', init_dir).items():
-                if key in policy:
-                    policy[key].extend(value)
-                else:
-                    policy[key] = value
-
+            if not args.exclude_initramfs:
+                initrmafs_hashes = hash_initramfs(f'{iso_dir}/live/initrd.img', init_dir)
+                merge_dict(initrmafs_hashes, hash_list)
+            if args.key:
+                # Only add files to the hash list also included in the initramfs.
+                # We need to do this because hashes in the allowlist take higher priority
+                # TODO: Check if this is actually required
+                for file_path in list(hash_list.keys()):
+                    path = os.path.join(squash_dir, file_path[1:])
+                    hashes = hash_files(path, rel_dir=squash_dir)
+                    merge_dict(hashes, hash_list)
+            else:
+                # Add all files to the hash list
+                hashes = hash_files(squash_dir)
+                merge_dict(hashes, hash_list)
             unmount(squash_dir)
             unmount(iso_dir)
 
@@ -85,12 +147,26 @@ def main():
             print(f"Something failed: {e.with_traceback()}")
             exit(1)
 
-    print(json.dumps({
+    ima_policy = {
         "meta": {
             "version": 2,
         },
-        "hashes": policy
-    }))
+        "hashes": hash_list
+    }
+    excllist = ["boot_aggregate"]
+    if args.exclude:
+        excllist.extend(args.exclude)
+
+    ima_policy_bundle = {
+        "ima_policy": base64.b64encode(json.dumps(ima_policy).encode()).decode(),
+        "excllist": excllist,
+        "checksum": "",
+    }
+    if args.key:
+        # Note that this is not the official Keylime format, but we use it for the Bridge
+        ima_policy_bundle["keyring"] = parse_keys(args.key)
+
+    print(json.dumps(ima_policy_bundle))
     exit(0)
 
 
