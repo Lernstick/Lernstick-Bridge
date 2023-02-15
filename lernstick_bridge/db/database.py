9a241d78accf2a001ad70c22c03c012b93048ffb
Thore Sommer <mail@thson.de>
2023-02-15 12:18:04 +0200

    db: add pre ping
    For more details see: https://docs.sqlalchemy.org/en/20/core/pooling.html#disconnect-handling-pessimistic
    
    run-pipe:docker

--- lernstick_bridge/db/database.py
+++ lernstick_bridge/db/database.py
@@ -7,7 +7,7 @@ from typing import Iterator
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import Session, sessionmaker
 
 from lernstick_bridge.config import config
 
@@ -18,7 +18,9 @@ if config.db_url.startswith("sqlite://"):
     _connect_args["check_same_thread"] = False
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args=_connect_args
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args=_connect_args
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
